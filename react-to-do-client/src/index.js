4e983ba2566b6a62e5fa8c7953d1b83c01dbc09b
herly <herlysarmiento@gmail.com>
2020-03-11 11:02:52 -0600

    remove the extra stuff to get project started

---
+++ react-to-do-client/src/index.js
@@ -0,0 +1,20 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+class App extends React.Component {
+    constructor() {
+        super()
+
+    }
+
+    render() {
+        return(
+            <div className="app">
+                <h1>Todo List</h1>
+            </div>
+        )
+    }
+}
+
+
+ReactDOM.render(<App />, document.getElementById('root'));
